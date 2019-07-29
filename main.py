a0630853b9edc20aeaa44ef0de52feeca996863d
dj <dj@itmojun.com>
2019-07-29 14:48:45 +0800

    优化音乐列表功能，现在可以从当前目录下的music目录中自动获取音乐列表

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import ctypes
 import random
+import os
+import sys
 
-music_list = ["白狐"
-    "笨小孩",
-    "此生不换",
-    "单身情歌",
-    "火苗",
-    "看透爱情看透你",
-    "看月亮爬上来",
-    "来生缘",
-    "郎的诱惑",
-    "天空之城",
-    "我和草原有个约定",
-    "真的爱你",
-    "醉美天下"
-]
+
+music_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], "music")
+
+try:
+    os.mkdir(music_path)
+except:
+    pass
+
+music_list = os.listdir(music_path)
+print(music_list)
+music_list = list(filter(lambda x: x[-4:].lower() in (".mp3", ), music_list))
+print(music_list)
+
+if len(music_list) == 0:
+    print("音乐列表为空，请将音乐文件放在music文件夹中！")
+    input("\n按回车键退出......")
+    sys.exit(1)
 
 music_name = random.choice(music_list)
-
 print("正在播放 %s" % music_name)
-
-ctypes.windll.winmm.mciSendStringW(r"open C:\Users\dengjun\Music\Music\%s.mp3 alias s" % music_name, None, 0, None)
+ctypes.windll.winmm.mciSendStringW(r"open %s alias s" % os.path.join(music_path, music_name), None, 0, None)
 ctypes.windll.winmm.mciSendStringW(r"play s repeat", None, 0, None)
 
 input("\n按回车键退出......")
